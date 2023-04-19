d27b530c9cb47686d82ef12dd21b92bbc436a331
charan7105 <saicharanvenna123@gmail.com>
2023-04-19 19:03:55 +0530

    add sc in starprint

--- starprint.java
+++ starprint.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+
 public class starprint 
 {
     public static void main(String[] args) 
     {
+        System.out.println("Enter your n Value");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
         for(int i=0; i<n; i++)  
         {
             for(int j=0; j<=i; j++)
