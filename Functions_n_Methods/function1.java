e522da084fcab2808437f0bf321087e37a793370
charan7105 <saicharanvenna123@gmail.com>
2023-06-07 21:03:43 +0530

    Functions

---
+++ Functions_n_Methods/function1.java
@@ -0,0 +1,18 @@
+package Functions_n_Methods;
+import java.util.Scanner;
+
+public class function1 {
+    public static int calculatesum(int num1, int num2) 
+    {
+        int sum = num1+num2;
+        return sum;
+    }
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = calculatesum(a, b);
+        System.out.println(sum);
+    }
+}
