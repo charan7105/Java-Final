cdb29be3d3efc895521a19604cbc7803762894f9
charan7105 <saicharanvenna123@gmail.com>
2023-06-07 23:32:39 +0530

    Add Comment

--- Functions_n_Methods/function1.java
+++ Functions_n_Methods/function1.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 public class function1 {
     public static int calculatesum(int num1, int num2) 
     {
-        int sum = num1+num2;
+        int sum = num1+num2; //Parameters or Formal Parameters
         return sum;
     }
     public static void main(String args[])
     {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in); //Actual Parameters or Arguments
         int a = sc.nextInt();
         int b = sc.nextInt();
         int sum = calculatesum(a, b);
