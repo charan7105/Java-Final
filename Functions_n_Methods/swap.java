dc6f6eee7eea539b384eecf7180c98b9fe106788
charan7105 <saicharanvenna123@gmail.com>
2023-06-09 15:49:06 +0530

    6.0.23 Methods again

--- Functions_n_Methods/swap.java
+++ Functions_n_Methods/swap.java
@@ -14,7 +14,28 @@ public class swap {
         int a = 10;
         int b = 5;
         swapno(a,b);
+
+    }
+}
+
+package Functions_n_Methods;
+import java.util.*;
+
+public class swap2 {
+    public static void swap(int a, int b) {
+        int swap = a;
+        a = b;
+        b = swap;
+        System.out.println("Now the values of a is:"+a);
+        System.out.println("Now the values of b is:"+b);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        swap(a,b);
         // System.out.println("a= "+a);
         // System.out.println("b= "+b);
     }
 }
+
