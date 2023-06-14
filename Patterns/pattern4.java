6f6c7da5edf357533806a52120fe4d34cc196bc9
charan7105 <saicharanvenna123@gmail.com>
2023-06-14 10:47:56 +0530

    stars

--- Patterns/pattern4.java
+++ Patterns/pattern4.java
@@ -1,20 +1,23 @@
 package Patterns;
 import java.util.*;
 
-public class increasing_traingle {
+public class pattern4 {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         System.out.println("How many Stars of Triangle you want?");
         int n = sc.nextInt();
         for(int i = 1;i<=n;i++)
         {
+            for(int j=i;j<=n-1;j++)
+            {
+                System.out.print("");
+            }
             for(int j=1;j<=i;j++)
             {
-                System.out.print("* ");
+                System.out.print(" *");
             }
             System.out.println();
         }
     }
 }
-
-
--- Patterns/starssquare.java
+++ Patterns/starssquare.java
@@ -7,9 +7,9 @@ public class starssquare {
         Scanner sc = new Scanner(System.in);
         System.out.println("How many Stars of Square you want?");
         int n = sc.nextInt();
-        for(int i = 0;i<n;i++)
+        for(int i = 1;i<=n;i++)
         {
-            for(int j=0;j<n;j++)
+            for(int j=1;j<=n;j++)
             {
                 System.out.print("* ");
             }
