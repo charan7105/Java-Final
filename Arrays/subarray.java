6406231fa1e2fd6df861e3a2d26b2382671f7bd4
charan7105 <saicharanvenna123@gmail.com>
2023-06-23 23:31:19 +0530

    comment

--- Arrays/subarray.java
+++ Arrays/subarray.java
@@ -4,6 +4,10 @@ public class subarray {
             int start = i;
             for(int j=i;j<Numbers.length;j++) {
                 int end = j;
+                //K is equal to end because it need to print min of one array. 
+                // Example => i=0; j=0; So now it will check to K
+                // K=0 K=0 It accepts and it will print
+                // Next it will increment to 1 then then it will fails so the loop stops, then it will again start from i.
                 for(int k=start;k<=end;k++){
                     System.out.print(Numbers[k]+" ");
                 }
