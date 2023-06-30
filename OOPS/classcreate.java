06a83dced13a451d47a10cffa1d5a9faf0f39ce8
charan7105 <saicharanvenna123@gmail.com>
2023-07-01 00:20:49 +0530

    Class Creations

---
+++ OOPS/classcreate.java
@@ -0,0 +1,26 @@
+package OOPS;
+
+public class classcreate {
+    public static void main(String[] args){
+    // Inside the main method, we create an object of the class "pen" called "p1" using the "new" keyword. This object represents a pen in our program.
+    pen p1 = new pen();
+    p1.ChangeColor("Yellow");
+    //This method is responsible for changing the color of the pen
+    System.out.println(p1.Color);
+   }
+}
+
+class pen{
+//Pen Class it has its own properties
+    String Color;
+    int Pointer;
+
+    // Pen class has two methods
+    void ChangeColor(String NewColor){
+        Color = NewColor;
+    }
+
+    void ChangePointer(int NewPointer){
+        Pointer = NewPointer;
+    }
+}
