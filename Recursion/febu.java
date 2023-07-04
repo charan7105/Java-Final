d0bd69db437dca96cb44da20c7bb52c83d3a737e
charan7105 <saicharanvenna123@gmail.com>
2023-07-04 14:57:26 +0530

    febu

---
+++ Recursion/febu.java
@@ -0,0 +1,19 @@
+package Recursion;
+
+public class febu {
+    public static int feb(int n){
+        if(n==1 || n==0){
+            return n;
+        }
+
+        int fnm1 = feb(n-1);
+        int fnm2 = feb(n-2);
+        int fn = fnm1+fnm2;
+        return fn;
+    }
+    
+    public static void main(String[] args){
+        int n = 5;
+        System.out.print(feb(n));
+    }
+}
