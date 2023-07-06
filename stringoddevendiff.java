b74a9e25b54c47a7883d5192d7f0ddbd435a255c
charan7105 <saicharanvenna123@gmail.com>
2023-07-06 15:06:14 +0530

    day 2

---
+++ stringoddevendiff.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class stringoddevendiff {
+    public static void findDiff(String num) 
+    {
+        int even = 0;
+        int odd = 0;
+
+        for (int i = 0; i < num.length(); i++) 
+        {
+            int digit = num.charAt(i);
+            
+            if (i % 2 == 0) 
+            {
+                even += digit; //Add Even
+            } else 
+            {
+                odd += digit; //Add Odd
+            }
+        }
+        int diff = even - odd;
+        System.out.println("Even Sum: " + even);
+        System.out.println("Odd Sum: " + odd);
+        System.out.println("Difference: " + Math.abs(diff));
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        String num = sc.nextLine();
+        findDiff(num);
+    }
+}
