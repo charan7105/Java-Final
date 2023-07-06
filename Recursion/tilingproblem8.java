94ef39b0130e9fba3feda1f3ce6fb306e8dd4a06
charan7105 <saicharanvenna123@gmail.com>
2023-07-07 00:16:37 +0530

    tiling problem

---
+++ Recursion/tilingproblem8.java
@@ -0,0 +1,23 @@
+package Recursion;
+
+public class tilingproblem8 
+{
+    
+    public static int ways(int n){ //2 * N Floor Size
+        //Base 
+        if(n==0 || n==1){
+            return 1;
+        }
+        //Vertical
+        int verticaltiles = ways(n-1);
+        //Horizontal
+        int horizontaltiles = ways(n-2);
+        
+        int TotalWays = verticaltiles + horizontaltiles;
+        return TotalWays;
+    }
+    public static void main(String[] args){
+        System.out.print(ways(4));
+    }
+}
+
