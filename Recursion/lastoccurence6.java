b2ce3137a30089b4b20aeb7a1ba31e9fd0e14703
charan7105 <saicharanvenna123@gmail.com>
2023-07-06 10:55:11 +0530

    lastoccurence

--- Recursion/lastoccurence6.java
+++ Recursion/lastoccurence6.java
@@ -1,21 +1,25 @@
 package Recursion;
 
-public class lastoccurence6 {
-
-    public static int findlast(int arr, int i, int key){
-        //Base
-        if(i >= 0){
+public class lastoccurence6{
+    public static int findLast(int[] arr, int i, int key) {
+        // Base case
+        if (i < 0) {
             return -1;
         }
-        //Method
-        if(arr[i]==key){
+
+        // Recursive case
+        if (arr[i] == key) {
             return i;
         }
-        //Reccurion
-        return findlast(arr,i-1,key);
+
+        // Recursive call
+        return findLast(arr, i - 1, key);
     }
-    public static void main(String[] args){
-        int arr[] = {12,34,523,1,5,5231,15,67,23,12,2,6,3,1,6,8,64,34};
-        System.out.println(arr,arr.length-1,5);
+
+    public static void main(String[] args) {
+        int[] arr = {12, 34, 6, 1, 5, 5231, 15, 67, 23, 12, 2, 6, 3, 1, 6, 8, 64, 34};
+        int key = 6;
+        int lastIndex = findLast(arr, arr.length - 1, key);
+        System.out.println("Last occurrence of " + key + " is at index: " + lastIndex);
     }
 }
