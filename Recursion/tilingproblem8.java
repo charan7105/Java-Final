34ca1495b4258fe3b1a7e7f5a2776e19669563fb
charan7105 <saicharanvenna123@gmail.com>
2023-07-07 00:19:56 +0530

    bracket

--- Recursion/tilingproblem8.java
+++ Recursion/tilingproblem8.java
@@ -3,9 +3,11 @@ package Recursion;
 public class tilingproblem8 
 {
     
-    public static int ways(int n){ //2 * N Floor Size
+    public static int ways(int n)
+    { //2 * N Floor Size
         //Base 
-        if(n==0 || n==1){
+        if(n==0 || n==1)
+        {
             return 1;
         }
         //Vertical
@@ -16,7 +18,8 @@ public class tilingproblem8
         int TotalWays = verticaltiles + horizontaltiles;
         return TotalWays;
     }
-    public static void main(String[] args){
+    public static void main(String[] args)
+    {
         System.out.print(ways(4));
     }
 }
