c3643dce29a6f370f6095db8e80194ed2931b262
charan7105 <saicharanvenna123@gmail.com>
2023-07-08 11:53:41 +0530

    mid error rectify

--- divideandconquer/mergesort1.java
+++ divideandconquer/mergesort1.java
@@ -49,15 +49,17 @@ public class mergesort1 {
             return;
         }
         //Method
-        int mid = si+(si+ei)/2;
+        int mid = (si+ei)/2;
         divide(arr,si,mid); //Left
         divide(arr,mid+1,ei); //Right
         merge(arr,si,ei,mid);
     }
     public static void main(String[] args){
         int arr[] = {2,5,3,6,7,2,10,8,9,1,4};
-        // printArray(arr);
+        System.out.println("Array Before Sorting");
+        printArray(arr);
         divide(arr,0,arr.length-1);
+        System.out.println("Array After Sorting");
         printArray(arr);
     }
 }
