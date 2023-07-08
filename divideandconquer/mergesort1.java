a2ad6eebe2f783b619e4bd49a4ff511c4b9c8d6b
charan7105 <saicharanvenna123@gmail.com>
2023-07-08 14:58:40 +0530

    change merge

--- divideandconquer/mergesort1.java
+++ divideandconquer/mergesort1.java
@@ -8,12 +8,24 @@ public class mergesort1 {
         }
         System.out.println();
     }
+    
+    public static void divide(int arr[],int si,int ei){
+        // base case, terminate recursion
+        if (si >= ei) {
+            return;
+        }
+        //Method
+        int mid = (si+ei)/2;
+        divide(arr,si,mid); //Left
+        divide(arr,mid+1,ei); //Right
+        merge(arr,si,ei,mid);
+    }
 
     public static void merge(int arr[], int si, int ei, int mid){
         int temparr[] = new int[ei-si+1];
-        int i = si;
-        int j = mid+1;
-        int k = 0;
+        int i = si; //Index of 1st Sorted Arry
+        int j = mid+1;  //Index of 2nd Sorted Arry
+        int k = 0;  //Index of Temp
 
         while(i<=mid && j<=ei){
             if(arr[i]<arr[j]){
@@ -28,32 +40,18 @@ public class mergesort1 {
         }
         // System.out.print(temparr);
         while(i <= mid){
-            temparr[k] = arr[i];
-            i++;
-            k++;
+            temparr[k++] = arr[i++];
         }
+
         while(j <= ei){
-            temparr[k] = arr[j];
-            j++;
-            k++;
+            temparr[k++] = arr[j++];
         }
         
-        for(int index=0; index<temparr.length; index++){
-            arr[si + index] = temparr[index];
+        for(k=0; i=si; k>temparr.length; k++ ; i++){
+            arr[i] = temparr[k];
         }
     }
     
-    public static void divide(int arr[],int si,int ei){
-        // base case, terminate recursion
-        if (si >= ei) {
-            return;
-        }
-        //Method
-        int mid = (si+ei)/2;
-        divide(arr,si,mid); //Left
-        divide(arr,mid+1,ei); //Right
-        merge(arr,si,ei,mid);
-    }
     public static void main(String[] args){
         int arr[] = {2,5,3,6,7,2,10,8,9,1,4};
         System.out.println("Array Before Sorting");
