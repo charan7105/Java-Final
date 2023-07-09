ad8efc8b1374fc7205f8bc60d52d041408cd055c
charan7105 <saicharanvenna123@gmail.com>
2023-07-09 14:22:41 +0530

    quicksort

---
+++ divideandconquer/quickSort11.java
@@ -0,0 +1,42 @@
+package divideandconquer;
+import java.util.*;
+
+public class quickSort11 {
+    
+    public static void QuickSort(int arr[],int si,int ei){
+        //Base 
+        if(si >= ei){
+            return;
+        }
+        int Pindx = Partition(arr,si,ei); //The Pivot Element's Index after it got Arranged 
+        QuickSort(arr,si,Pindx-1); //Left Side One
+        QuickSort(arr,si,Pindx-1); //Right Side One
+    }
+
+    public static int Partition(int arr[], int si, int ei){
+        //Pivot Element
+        int Pivot = arr[ei];
+        int i=si-1; //i = -1
+
+        for(int j=si; j<ei;j++){
+            if(arr[j]<= Pivot){
+                i++;
+                //Swap The Non Pivot Elements
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+        //Swap the Pivot Element
+        i++;
+        int temp = Pivot;
+        Pivot = arr[i];
+        arr[i] = temp;
+        return i;
+    }
+    public static void main(String[] args){
+        int arr[] = {2,3,9,8,2,5};
+        QuickSort(arr,0,arr.length-1);
+        System.out.print(Arrays.toString(arr));
+    }
+}
